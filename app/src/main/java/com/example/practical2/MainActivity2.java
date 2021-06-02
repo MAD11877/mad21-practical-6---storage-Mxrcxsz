b53c7dfc5531f351aad541c9cb18bbcc982a195d
Mxrcxsz <s10208495@connect.np.edu.sg>
2021-06-02 13:08:22 +0800

    Updated week 6 practical

---
+++ app/src/main/java/com/example/practical2/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.practical2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent in = getIntent();
+        TextView display = findViewById(R.id.textView2);
+        display.setText(in.getStringExtra("userInput"));
+    }
+}
